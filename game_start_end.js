f734724c22de8c513d95c823b8be7a882ed481b7
Ben Schell <ben@benschell.org>
2012-05-23 15:02:56 -0400

    Adjusting hourly task to happen at 12AM, 8AM, 4PM (rather than hourly).

--- game_start_end.js
+++ game_start_end.js
@@ -10,6 +10,12 @@ fs.readdirSync(dir).forEach(function(file){
 	require('./app/models/'+file);
 });
 
+var d = new Date()
+  , h = d.getHours()
+if(h != 0 && h != 8 && h != 16){
+	process.exit(0);
+}
+
 var Game = mongoose.model('Game');
 Game.endGames(function(){
 	setTimeout(function(){
