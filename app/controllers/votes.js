20d3543ba7750148d5fcf3e3d5cef86fbaa142d4
Ben Schell <ben@benschell.org>
2012-06-07 13:28:23 -0400

    Changing vote export to handle request timeouts in a MUCH better way for #39.

--- app/controllers/votes.js
+++ app/controllers/votes.js
@@ -1,16 +1,26 @@
 var utilities = require('./utilities')
   , mongoose = require('mongoose');
 
-app.get('/votes/export', utilities.checkAdmin, function(req, res){
+app.get('/votes/export', utilities.checkAdmin, function(req, res, next){
 	// Export all game data as a CSV
 	var Game = mongoose.model('Game')
 	  , Round = mongoose.model('Round')
 	  , Vote = mongoose.model('Vote');
-	Game.find().populate('opponents').asc('startTime').run(function(err, games){
+
+/* 	res.contentType('.csv'); */
+
+	var csv = 'player ID, player email, player type, game ID, round #, vote date, player\'s vote, info type this round, player\'s info this round, opponent\'s info this round\n';
+	
+	res.writeHead(200, {
+		'Content-Type': 'text/csv',
+		'Content-Disposition': 'attachment;filename=export.csv'
+	});
+
+	Game.find({}, ['_id', 'rounds', 'startTime', 'opponents', 'rounds']).populate('opponents').populate('rounds', ['number', 'votes'])/*.asc('startTime')*/.run(function(err, games){
 		var game = null
 		  , round = null
 		  , numVotes = 0
-		  , csv = 'player ID, player email, player type, game ID, round #, vote date, player\'s vote, info type this round, player\'s info this round, opponent\'s info this round\n'
+		  , start = Date.now()
 		  , handleVote = function(game, round){
 				return function(err, vote){
 					// Handle the vote
@@ -24,31 +34,27 @@ app.get('/votes/export', utilities.checkAdmin, function(req, res){
 						addToCSV += game.opponents[1].getOpponentProfileForCSV(game.startTime);
 					}
 					addToCSV += '\n';
-					csv += addToCSV;
+					res.write(addToCSV);
 					
 					if(--numVotes == 0){
-						res.attachment('export.csv');
-						res.contentType('csv');
-						res.send(csv);
-						return;
+						util.log('done! '+Date.now());
+						util.log('total time: '+((Date.now()-start)/1000)+'s');
+						res.end();
 					}
 				}
-			}
-		  , handleRound = function(game){
-				return function(err, round){
-					// Handle the round!
-					for(var i=0; i<round.votes.length; i++){
-						var vote = round.votes[i];
-						numVotes++;
-						Vote.findById(vote).populate('player').run(handleVote(game, round));
-					}
-				};
 			};
+		util.log('starting: '+Date.now());
+		res.write(csv);
+		
 		for(var i=0; i<games.length; i++){
 			game = games[i];
 			for(var j=0; j<game.rounds.length; j++){
 				round = game.rounds[j];
-				Round.findById(round).run(handleRound(game));
+				numVotes += round.votes;
+				for(var k=0; k<round.votes.length; k++){
+					var vote = round.votes[k];
+					Vote.findById(vote).populate('player').run(handleVote(game, round));
+				}
 			}
 		}
 	});
