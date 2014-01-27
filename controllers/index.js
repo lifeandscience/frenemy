eced1ee83643b38d262a2144a8cc9ca9f3b35a65
Benjamin Schell <ben@benschell.org>
2014-01-26 21:00:37 -0500

    Redirecting to /play in the case of a user who is logged in and signed up for this one.

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,22 @@
 var util = require('util')
-  , less = require('less');
+  , less = require('less')
+  , auth = require('./auth');
 
 app.get('/', function(req, res){
+	console.log('user: ', req.session.user);
+	if(req.session.user){
+		if(req.session.user.role >= 10){
+			return res.redirect('/play');
+		}
+		if(req.session.user.experimonths){
+			for(var i=0; i<req.session.user.experimonths.length; i++){
+				if(req.session.user.experimonths[i].kind.toString() == auth.clientID){
+					// Found that the user is in an Experimonth with this Kind
+					return res.redirect('/play');
+				}
+			}
+		}
+	}
 	res.render('index', {title: 'Welcome to Frenemy!', util: util});
 });
 app.get('/login', function(req, res){
