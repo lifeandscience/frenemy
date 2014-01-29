62717740e9404bd0144ce24a0d3f5759a5e05bba
Benjamin Schell <ben@benschell.org>
2014-01-29 11:18:50 -0500

    New payoff structure.

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
 	points: {
 		// 'friend' + 'friend'
-		winningTie: process.env.pointsForFriendlyTie || 15
+		winningTie: process.env.pointsForFriendlyTie || 30
 		// 'friend' in 'friend' + 'enemy'
-	  , loss: process.env.pointsForLoss || -5
+	  , loss: process.env.pointsForLoss || -10
 		// 'enemy' in 'friend' + 'enemy'
-	  , win: process.env.pointsForWin || 25
+	  , win: process.env.pointsForWin || 40
 		// 'enemy' + 'enemy'
-	  , losingTie: process.env.pointsForUnfriendlyTie || 5
+	  , losingTie: process.env.pointsForUnfriendlyTie || 0
 	}
   , admins: ['ben.schell@gmail.com']
 };
