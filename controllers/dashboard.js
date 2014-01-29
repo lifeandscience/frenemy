540b62937650122dd2960db1577157a172b3069e
Benjamin Schell <ben@benschell.org>
2014-01-29 11:46:08 -0500

    Try fixing dashboard (/play) display

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -15,27 +15,23 @@ app.get('/play', auth.authorize(1, 0, null, true), function(req, res){
 			console.log('no players found for current user: ', err);
 			return res.redirect('/');
 		}
-		async.sortBy(players, function(player, callback){
-			callback(null, player.lastPlayed);
-		}, function(err, players){
-			var player = players[players.length-1];
-			Game.find({_id: {$in: player.games}}).exec(function(err, games){
-				async.filter(games, function(item, callback){
-					callback(!item.completed);
-				}, function(completedGames){
-					if(completedGames.length == 0 && games.length > 0){
-						async.sortBy(games, function(game, callback){
-							callback(null, game.startTime);
-						}, function(err, games){
-							return res.redirect('/game/'+games[games.length-1]._id);
-						});
-						return;
-					}
-					if(completedGames.length == 1){
-						return res.redirect('/game/'+completedGames[0]._id);
-					}
-					return res.render('dashboard', {title: 'Your Profile', player: player, games: completedGames});
-				});
+
+		Game.find({opponents: {$in: players}}).exec(function(err, games){
+			async.filter(games, function(item, callback){
+				callback(!item.completed);
+			}, function(incompleteGames){
+				if(incompleteGames.length == 0 && games.length > 0){
+					async.sortBy(games, function(game, callback){
+						callback(null, game.startTime);
+					}, function(err, games){
+						return res.redirect('/game/'+games[games.length-1]._id);
+					});
+					return;
+				}
+				if(incompleteGames.length > 0){
+					return res.redirect('/game/'+incompleteGames[0]._id);
+				}
+				return res.render('dashboard', {title: 'Your Profile', player: player, games: games});
 			});
 		});
 	});
