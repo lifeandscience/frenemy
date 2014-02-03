0095fcc77ec64ad002e154842e3cd2edcfdc4852
Benjamin Schell <ben@benschell.org>
2014-02-03 15:54:39 -0500

    Copying mongo.sh from EM to Frenemy for more usefulness

--- bin/mongo.sh
+++ bin/mongo.sh
@@ -37,19 +37,34 @@ shell)
 export)
 	mongodump --host $HOST --port $PORT --username $USERNAME --password $PASSWORD --db $APPID --out bin/mongo.$2
 	;;
-import)
+importToLocal)
 	echo "Importing from $2"
 	DATE=`date "+%Y-%m-%d-%H-%M-%S"`
-	OLD_NAME="frenemy-$DATE"
-	mongo frenemy --eval "db.copyDatabase('frenemy', '$OLD_NAME')"
-	mongo frenemy --eval "db.dropDatabase()"
-	mongorestore --host localhost -d frenemy "bin/mongo.$2/$APPID"
+	OLD_NAME="experimonth-$DATE"
+	mongo experimonth --eval "db.copyDatabase('experimonth', '$OLD_NAME')"
+	# mongo experimonth --eval "db.collection.remove()"
+	mongo experimonth --eval "var collectionNames = db.getCollectionNames(); for(var i = 0, len = collectionNames.length; i < len ; i++){ var collectionName = collectionNames[i]; if(collectionName.indexOf('system') == -1){ db[collectionName].drop(); } }"
+	mongorestore --host localhost -d experimonth "bin/mongo.$2/$APPID"
+	;;
+importToRemote)
+	echo "Importing into $2 from bin/mongo.$3/$4 in 15s..."
+	echo "THIS WILL DESTROY ALL DATA ON $2!!!"
+	sleep 15s
+	echo "Removing all collections except user on $HOST:$PORT/$APPID in 5s"
+	sleep 5s
+	# mongo $HOST:$PORT/$APPID --username $USERNAME --password $PASSWORD --eval "db.collection.remove()"
+	mongo $HOST:$PORT/$APPID --username $USERNAME --password $PASSWORD --eval "var collectionNames = db.getCollectionNames(); for(var i = 0, len = collectionNames.length; i < len ; i++){ var collectionName = collectionNames[i]; if(collectionName.indexOf('system') == -1){ db[collectionName].drop(); } }"
+
+	echo "Executing mongorestore on $HOST:$PORT/$APPID from bin/mongo.$3/$4 in 5s"
+	echo "mongorestore --drop --host $HOST --port $PORT -d $APPID -u $USERNAME -p $PASSWORD bin/mongo.$3/$4"
+	sleep 5s
+	mongorestore --drop --host $HOST --port $PORT -d $APPID -u $USERNAME -p $PASSWORD "bin/mongo.$3/$4"
 	;;
 clear)
 	echo "Clearing database..."
 	DATE=`date "+%Y-%m-%d-%H-%M-%S"`
-	OLD_NAME="frenemy-$DATE"
-	mongo frenemy --eval "db.copyDatabase('frenemy', '$OLD_NAME'); db.dropDatabase();"
+	OLD_NAME="experimonth-$DATE"
+	mongo experimonth --eval "db.copyDatabase('experimonth', '$OLD_NAME'); db.dropDatabase();"
 	echo "Done."
 	;;
 esac
